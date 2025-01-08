6452553c7c320f2261059069ded35152b497189a
ishwarya-18 <ishwarya.ad23@bitsathy.ac.in>
2025-01-08 09:40:14 +0530

    Initial commit for my-todo-app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login/Login';
+import TaskManager from './TodoList/TodoList'; // Assuming TaskManager is the To-Do list page component
+import AdminPage from './Admin/Admin';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/todo" element={<TaskManager />} />
+                <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
+
+
+
